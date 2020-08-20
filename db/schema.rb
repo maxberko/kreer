e18a416a00bdc83f35d3efcac1ea4b8868f2ac09
Manon de Demandolx dedons <manondedemandolx@hotmail.fr>
2020-08-20 12:37:07 +0200

    creatingnewcolumntotest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_161928) do
+ActiveRecord::Schema.define(version: 2020_08_20_103546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_161928) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "jobtype"
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
