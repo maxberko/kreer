5f180366a0261e15e3ddbc767874411facd3d6b4
maxime berkowicz <maximeberkowicz@gmail.com>
2020-08-20 12:16:15 +0200

    second iteration of controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class TestsController < ApplicationController
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    # todo
+  end
+
+  def new
+    # todo
+  end
+
+  def create
+    # todo
+  end
 end
